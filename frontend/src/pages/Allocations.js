c7937639c47dbecefd89ade44074d13de9c414f5
aarathink <aarathinarayanan99@gmail.com>
2025-04-25 21:33:35 +1000

    updated Allocations page with placeholders for features

--- frontend/src/pages/Allocations.js
+++ frontend/src/pages/Allocations.js
@@ -4,8 +4,10 @@ import {
   Box,
   Container,
   Heading,
-  Text,
-  Button,
+  SimpleGrid,
+  Divider,
+  AspectRatio,
+  Center,
 } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -13,6 +15,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Allocations = () => {
   const navigate = useNavigate();
+  const handleInProgress = (section) => alert(`${section} page is in progress`);
 
   return (
     <>
@@ -20,7 +23,63 @@ const Allocations = () => {
       <Box bg="gray.100" minH="100vh" py={10}>
         <Container maxW="7xl" bg="white" p={8} borderRadius="lg" boxShadow="lg">
           <Heading size="lg" mb={4}>Allocations</Heading>
-          <Text mb={6}>This page is under development. Please check back soon!</Text>
+          <Divider mb={6} />
+
+          <SimpleGrid columns={[1, 3]} spacing={6} justifyItems="center">
+            {/* Allocate Students */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => handleInProgress('Allocate Students')}
+                bg="white"
+                border="1px solid"
+                borderColor="gray.300"
+                borderRadius="md"
+                boxShadow="sm"
+                _hover={{ boxShadow: 'md' }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Allocate Students</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
+
+            {/* Manual Override */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => handleInProgress('Manual Override')}
+                bg="white"
+                border="1px solid"
+                borderColor="gray.300"
+                borderRadius="md"
+                boxShadow="sm"
+                _hover={{ boxShadow: 'md' }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Manual Override</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
+
+            {/* Reallocation */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => handleInProgress('Reallocation')}
+                bg="white"
+                border="1px solid"
+                borderColor="gray.300"
+                borderRadius="md"
+                boxShadow="sm"
+                _hover={{ boxShadow: 'md' }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Reallocation</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
+          </SimpleGrid>
         </Container>
       </Box>
       <Footer />
--- frontend/src/pages/TeacherDashboard.js
+++ frontend/src/pages/TeacherDashboard.js
@@ -5,8 +5,10 @@ import {
   Container,
   Heading,
   Text,
+  Divider,
   SimpleGrid,
-  VStack,
+  AspectRatio,
+  Center,
 } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -25,55 +27,77 @@ const TeacherDashboard = () => {
       <Box bg="gray.100" minH="100vh" py={10}>
         <Container maxW="7xl" bg="white" p={8} borderRadius="lg" boxShadow="lg">
           <Heading size="lg" mb={2}>Teacher Dashboard</Heading>
+          <Divider mb={6} />
           <Text mb={6}>
             Welcome to ClassForge! View student analytics, track feedback, and manage classroom allocations here.
           </Text>
 
-          <SimpleGrid columns={[1, 3]} spacing={6}>
-            {/* Students tile */}
-            <Box
-              as="button"
-              onClick={() => handleInProgress('Students')}
-              border="1px"
-              borderColor="gray.300"
-              borderRadius="md"
-              height="150px"
-              _hover={{ bg: 'gray.50' }}
-            >
-              <VStack h="100%" justify="center">
-                <Heading size="md">Students</Heading>
-              </VStack>
-            </Box>
+          <SimpleGrid columns={[1, null, 3]} spacing={6} justifyItems="center">
+            {/* Students */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => handleInProgress('Students')}
+                bg="white"
+                borderWidth="1px"
+                borderColor="gray.200"
+                borderRadius="lg"
+                boxShadow="base"
+                transition="all 0.2s"
+                _hover={{
+                  boxShadow: 'md',
+                  borderColor: 'gray.300',
+                }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Students</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
 
-            {/* Allocations tile */}
-            <Box
-              as="button"
-              onClick={() => navigate('/teacher/allocations')}
-              border="1px"
-              borderColor="gray.300"
-              borderRadius="md"
-              height="150px"
-              _hover={{ bg: 'gray.50' }}
-            >
-              <VStack h="100%" justify="center">
-                <Heading size="md">Allocations</Heading>
-              </VStack>
-            </Box>
+            {/* Allocations */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => navigate('/teacher/allocations')}
+                bg="white"
+                borderWidth="1px"
+                borderColor="gray.200"
+                borderRadius="lg"
+                boxShadow="base"
+                transition="all 0.2s"
+                _hover={{
+                  boxShadow: 'md',
+                  borderColor: 'gray.300',
+                }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Allocations</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
 
-            {/* Classes tile */}
-            <Box
-              as="button"
-              onClick={() => handleInProgress('Classes')}
-              border="1px"
-              borderColor="gray.300"
-              borderRadius="md"
-              height="150px"
-              _hover={{ bg: 'gray.50' }}
-            >
-              <VStack h="100%" justify="center">
-                <Heading size="md">Classes</Heading>
-              </VStack>
-            </Box>
+            {/* Classes */}
+            <AspectRatio ratio={1} maxW="240px" w="100%">
+              <Box
+                as="button"
+                onClick={() => handleInProgress('Classes')}
+                bg="white"
+                borderWidth="1px"
+                borderColor="gray.200"
+                borderRadius="lg"
+                boxShadow="base"
+                transition="all 0.2s"
+                _hover={{
+                  boxShadow: 'md',
+                  borderColor: 'gray.300',
+                }}
+              >
+                <Center h="100%">
+                  <Heading size="md">Classes</Heading>
+                </Center>
+              </Box>
+            </AspectRatio>
           </SimpleGrid>
         </Container>
       </Box>
